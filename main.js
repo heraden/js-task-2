c8f159fd92c47d5732f0a4fda9d9427a5d898cb0
heraden <127416579+heraden@users.noreply.github.com>
2023-04-21 19:32:04 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-<!DOCTYPE html> 
-<html lang="en"> 
-<head> 
-    <meta charset="UTF-8"> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Document</title> 
-</head> 
-<div id="main" style="display: flex; flex-direction: column; width: 400px;"> 
-<label>Enter Kilometres</label> 
-<input type="text" id="kilometr"> 
- 
-<button id="bottom" style="margin-top: 30px;">Sign in</button> 
-    <script src="./nem.js"></script> 
-</body> 
-</html
+document.getElementById("bottom").addEventListener("click", function () { 
+    event.preventDefault(); 
+    let kilometres = document.getElementById("kilometr").value; 
+   
+  const end =   5 + kilometres * 0.25   
+  return console.log(end) 
