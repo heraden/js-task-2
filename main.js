fafcbaa8fe86d88f468e37fc0ac3f46eef87b210
heraden <127416579+heraden@users.noreply.github.com>
2023-04-21 19:30:02 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function customer() { 
-    const kilometer = prompt("Enter kilometer"); 
-return 5 + kilometer * 0.25  
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title> 
+</head> 
+<div id="main" style="display: flex; flex-direction: column; width: 400px;"> 
+<label>Enter Kilometres</label> 
+<input type="text" id="kilometr"> 
  
-} 
-const customerInfo =  customer(); 
-console.log(customerInfo);
+<button id="bottom" style="margin-top: 30px;">Sign in</button> 
+    <script src="./nem.js"></script> 
+</body> 
+</html
